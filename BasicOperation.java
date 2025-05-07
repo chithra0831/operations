805c2325ef745b9b4eed632d07dc3d1204f368e2
chithra0831 <chithrasm0831@gmail.com>
2025-05-07 15:56:28 +0530

    saved

---
+++ BasicOperation.java
@@ -0,0 +1,16 @@
+public class BasicOperation {
+    public BasicOperation() {
+    }
+ 
+    public static void main(String[] var0) {
+       byte var1 = 15;
+       byte var2 = 5;
+       int var3 = var1 + var2;
+       int var4 = var1 - var2;
+       int var5 = var1 * var2;
+       System.out.println("Addition:" + var3);
+       System.out.println("Subtraction:" + var4);
+       System.out.println("Multiplication:" + var5);
+    }
+ }
+ 
